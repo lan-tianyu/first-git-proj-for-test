5fa19c1160efe13f463cae44c1e8e2ed23e39d5a
lantianyu <lantianyu@jd.com>
2019-12-12 20:19:27 +0800

    20191212--done

--- leetCode/lengthOfLongestSubstring.py
+++ leetCode/lengthOfLongestSubstring.py
@@ -1,37 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        count = 0
-        first = -1
-        last = 0
         map_d = {}
+        base = -1
+        count = 0
         for i, e in enumerate(s):
-            if e in map_d:
-                # 考虑 abba， abccba这种场景
-                first = max(map_d.get(e), first)
-                map_d[e] = i
-                last = i
-            else:
-                map_d[e] = i
-                last = i
-            count = max(count, last - first)
+            base = max(base, map_d.get(e, -1))
+            count = max(count, i - base)
+            map_d[e] = i
         return count
 
 
-class TestSolution():
-    def test_lengthOfLongestSubstring(self):
-        s = Solution()
-        ss1 = 'dddd'
-        print(s.lengthOfLongestSubstring(ss1))
-        ss1 = 'abcdabc'
-        print(s.lengthOfLongestSubstring(ss1))
-        ss1 = 'abcabcdaef'
-        print(s.lengthOfLongestSubstring(ss1))
-        ss1 = 'pwwkew'
-        print(s.lengthOfLongestSubstring(ss1))
-        ss1 = 'abba'
-        print(s.lengthOfLongestSubstring(ss1))
-
-
-if __name__ == '__main__':
-    test = TestSolution()
-    test.test_lengthOfLongestSubstring()
+solution = Solution()
+assert solution.lengthOfLongestSubstring('abcde') == 5
+assert solution.lengthOfLongestSubstring('abcdeabc') == 5
+assert solution.lengthOfLongestSubstring('abccbad') == 4
+assert solution.lengthOfLongestSubstring('abcdcba') == 4
+assert solution.lengthOfLongestSubstring('pwwkew') == 3
